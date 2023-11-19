c8e1409f03406caa062b9ded5c7118c9125b728c
truebrr <123203952+truebrr@users.noreply.github.com>
2023-11-19 14:45:00 -0300

    commit 1

---
+++ assets/scripts.js
@@ -0,0 +1,24 @@
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach( (entry) => { 
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.principal, header')
+
+elements.forEach( (element) => myObserver.observe(element) )
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../Portifólio/Templates/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "../Portifólio/Templates/close_white_36dp.svg"
+    }
+}
